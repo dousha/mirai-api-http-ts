6da171ab8edebc5b6b1225da6b9acadb9f7277c4
dousha99 <lijiahao34@live.com>
2020-11-17 14:30:26 +0800

    add docs

--- src/services/OutboundMessagingService.ts
+++ src/services/OutboundMessagingService.ts
@@ -2,6 +2,7 @@ import { SessionAuthenticationService } from './SessionAuthenticationService';
 import { HttpService } from './HttpService';
 import { OutboundMessageChain } from '../objects/OutboundMessageChain';
 import { BasicResponse } from '../objects/ServerResponse';
+import { AxiosResponse } from 'axios';
 
 export class OutboundMessagingService {
 	constructor(private readonly auth: SessionAuthenticationService, private readonly http: HttpService) {
@@ -9,7 +10,16 @@ export class OutboundMessagingService {
 		this.http = http;
 	}
 
-	public sendToFriend(num: number, msg: OutboundMessageChain) {
+	/**
+	 * 发送好友消息。
+	 *
+	 * @param {number} num 好友 QQ 号
+	 * @param {OutboundMessageChain} msg 消息链
+	 * @returns {Promise<AxiosResponse<BasicResponse>>}
+	 * @see OutboundMessageChain
+	 * @since 0.0.1
+	 */
+	public sendToFriend(num: number, msg: OutboundMessageChain): Promise<AxiosResponse<BasicResponse>> {
 		return this.auth.obtainToken().then(token =>
 			this.http.post<BasicResponse>('/sendFriendMessage', {
 				sessionKey: token,
@@ -18,7 +28,16 @@ export class OutboundMessagingService {
 			}));
 	}
 
-	public sendToGroup(num: number, msg: OutboundMessageChain) {
+	/**
+	 * 发送群消息。
+	 *
+	 * @param {number} num 群号
+	 * @param {OutboundMessageChain} msg 消息链
+	 * @returns {Promise<AxiosResponse<BasicResponse>>}
+	 * @see OutboundMessageChain
+	 * @since 0.0.1
+	 */
+	public sendToGroup(num: number, msg: OutboundMessageChain): Promise<AxiosResponse<BasicResponse>> {
 		return this.auth.obtainToken().then(token =>
 			this.http.post<BasicResponse>('/sendGroupMessage', {
 				sessionKey: token,
@@ -27,7 +46,17 @@ export class OutboundMessagingService {
 			}));
 	}
 
-	public sendToTemp(groupId: number, recipientId: number, msg: OutboundMessageChain) {
+	/**
+	 * 发送临时会话消息。
+	 *
+	 * @param {number} groupId 群号
+	 * @param {number} recipientId 接收方 QQ 号
+	 * @param {OutboundMessageChain} msg 消息链
+	 * @returns {Promise<AxiosResponse<BasicResponse>>}
+	 * @see OutboundMessageChain
+	 * @since 0.0.1
+	 */
+	public sendToTemp(groupId: number, recipientId: number, msg: OutboundMessageChain): Promise<AxiosResponse<BasicResponse>> {
 		return this.auth.obtainToken().then(token =>
 			this.http.post<BasicResponse>('/sendTempMessage', {
 				sessionKey: token,
@@ -37,11 +66,28 @@ export class OutboundMessagingService {
 			}));
 	}
 
-	public revokeMessage(messageId: number) {
+	/**
+	 * 撤回消息。
+	 *
+	 * @param {number} messageId 消息 ID
+	 * @returns {Promise<AxiosResponse<BasicResponse>>}
+	 * @since 0.0.1
+	 */
+	public revokeMessage(messageId: number): Promise<AxiosResponse<BasicResponse>> {
 		return this.auth.obtainToken().then(token =>
 			this.http.post<BasicResponse>('/recall', {
 				sessionKey: token,
 				target: messageId,
 			}));
+	}
+
+	/**
+	 * 获取当前机器人的 QQ 号。
+	 *
+	 * @returns {number}
+	 * @since 0.1.3
+	 */
+	public get account(): number {
+		return this.auth.account;
 	}
 }
